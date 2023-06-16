b74b8fd5a12f9129ea6c7673b1bda4a770cac3df
Dark Network <90391646+0x01369@users.noreply.github.com>
2023-06-16 23:16:43 +0300

    Update api_credentials_extractor.py

--- api_credentials_extractor.py
+++ api_credentials_extractor.py
@@ -1,31 +1,48 @@
 import requests
+import json
 
 api_token = "null"
-api_url = f"https://api.pipedrive.com/v1/users?api_token={api_token}"
+base_url = "https://api.pipedrive.com/v1"
+endpoints = ["users", "deals", "persons", "notes", "products", "organizations"]
 
-# API isteği ile kullanıcıları al
-response = requests.get(api_url)
-response_data = response.json()
 
-# Check if the response is empty
-if not response_data:
-    print("Could not get a valid response from the API.")
-    exit(1)
+def make_api_request(endpoint):
+    url = f"{base_url}/{endpoint}?api_token={api_token}"
+    response = requests.get(url)
+    return response.json()
 
-# Operations that extract user information
-user_count = len(response_data['data'])
-if user_count == 0:
-    print("User not found.")
-    exit(0)
 
-print("user information:")
-print("----------------------")
+def print_user_info(user_data):
+    user_count = len(user_data["data"])
 
-for i in range(user_count):
-    username = response_data['data'][i]['name']
-    email = response_data['data'][i]['email']
+    if user_count == 0:
+        print("User not found.")
+        exit(0)
 
-    print(f"User name: {username}")
-    print(f"E-Mail: {email}")
+    print("User information:")
     print("----------------------")
 
+    for i in range(user_count):
+        username = user_data["data"][i]["name"]
+        email = user_data["data"][i]["email"]
+        phone = user_data["data"][i].get("phone", "N/A")
+        print(f"User name: {username}")
+        print(f"E-Mail: {email}")
+        print(f"Phone: {phone}")
+        print("----------------------")
+
+
+for endpoint in endpoints:
+    response_data = make_api_request(endpoint)
+
+    if not response_data:
+        print("Could not get a valid response from the API.")
+        exit(1)
+
+    print(f"API request result ({endpoint}):")
+    print("----------------------")
+    print(json.dumps(response_data, indent=4))
+    print("----------------------")
+
+user_response_data = make_api_request("users")
+print_user_info(user_response_data)
