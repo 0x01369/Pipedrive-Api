519bf6943f31bd543d87854e08e8e2b7c4058aca
Dark Network <90391646+0x01369@users.noreply.github.com>
2023-06-16 23:08:17 +0300

    Update api_credentials_extractor.sh

--- api_credentials_extractor.sh
+++ api_credentials_extractor.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
 
 api_token="null"
-api_url="https://api.pipedrive.com/v1/users?api_token=$api_token"
+base_url="https://api.pipedrive.com/v1"
+endpoints=("users" "deals" "persons" "notes" "products" "organizations")
 
-# API request fetching users
-response_data=$(curl -s -X GET "$api_url")
 
-# Checking if the response is empty
-if [[ -z "$response_data" ]]; then
-    echo "Could not get a valid response from the API."
-    exit 1
-fi
+function make_api_request() {
+  local endpoint=$1
+  local url="$base_url/$endpoint?api_token=$api_token"
+  curl -s -X GET "$url"
+}
 
-# Operations that extract user information
-user_count=$(echo "$response_data" | jq '.data | length')
-if [[ $user_count -eq 0 ]]; then
+
+function print_user_info() {
+  local user_data=$1
+  local user_count=$(echo "$user_data" | jq '.data | length')
+
+  if [[ $user_count -eq 0 ]]; then
     echo "User not found."
     exit 0
-fi
+  fi
 
-echo "User information:"
-echo "----------------------"
+  echo "user information:"
+  echo "----------------------"
 
-for i in $(seq 0 $(expr $user_count - 1)); do
-    username=$(echo "$response_data" | jq -r ".data[$i].name")
-    email=$(echo "$response_data" | jq -r ".data[$i].email")
-
+  for i in $(seq 0 $(expr $user_count - 1)); do
+    username=$(echo "$user_data" | jq -r ".data[$i].name")
+    email=$(echo "$user_data" | jq -r ".data[$i].email")
+    phone=$(echo "$user_data" | jq -r ".data[$i].phone")
     echo "User name: $username"
     echo "E-Mail: $email"
+    echo "Phone: $phone"
     echo "----------------------"
+  done
+}
+
+
+for endpoint in "${endpoints[@]}"; do
+  response_data=$(make_api_request "$endpoint")
+
+  if [[ -z "$response_data" ]]; then
+    echo "Could not get a valid response from the API."
+    exit 1
+  fi
+
+  echo "API request result ($endpoint):"
+  echo "----------------------"
+  echo "$response_data"
+  echo "----------------------"
 done
 
+
+user_response_data=$(make_api_request "users")
+print_user_info "$user_response_data"
